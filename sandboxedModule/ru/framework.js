9adb80ca2359acb9fc9d5d533e2a8aa87c05101f
Greemjoy <dante3@bigmir.net>
2016-03-24 21:45:49 +0200

    Update framework.js

--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -4,23 +4,33 @@
 // приложением интерфейс. Читайте README.md в нем задания.
 
 // Фреймворк может явно зависеть от библиотек через dependency lookup
-var fs = require('fs'),
-    vm = require('vm');
+var fs   = require('fs'  ),
+    vm   = require('vm'  ),
+    util = require('util');
 
-// Создаем контекст-песочницу, которая станет глобальным контекстом приложения
-var context = { module: {}, console: console };
-context.global = context;
-var sandbox = vm.createContext(context);
+// Чоздаем контекст-песочницу, которая станет глобальным контекстом приложения
+var context = {
+
+  module	: {},
+  console	: console,
+
+  setTimeout  : setTimeout,
+  setInterval : setInterval,
+
+  util : util,
+
+};
+
+context.global = context; var sandbox = vm.createContext(context);
 
 // Читаем исходный код приложения из файла
 var fileName = './application.js';
 fs.readFile(fileName, function(err, src) {
   // Тут нужно обработать ошибки
-  
   // Запускаем код приложения в песочнице
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
-  
+
   // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
   // сохранить в кеш, вывести на экран исходный код приложения и т.д.
 });
